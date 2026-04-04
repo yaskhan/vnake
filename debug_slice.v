afd0705fc3aa5ba32f1b953f3be34f0939015674
Yaskhan <yaskhan@yandex.ru>
2026-04-04 18:44:27 +0500

    Delete debug_slice.v

--- debug_slice.v
+++
@@ -1,9 +0,0 @@
-import translator
-
-fn main() {
-    source := 'l = [1, 2, 3]
-l[1:2] = [4]'
-    mut t := translator.new_translator()
-    res := t.translate(source, 'test.py')
-    println(res)
-}
